c1474e4a40996558e9f9f8e871622564fd3f4dab
Tyler Smith <tjismith13@icloud.com>
2020-11-02 18:35:15 -0500

    Update test opMode with intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpModes/DrivetrainTeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpModes/DrivetrainTeleOpTest.java
@@ -4,20 +4,31 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 @TeleOp
 public class DrivetrainTeleOpTest extends OpMode {
 
     Drivetrain drivetrain;
+    Intake intake;
 
     @Override
     public void init() {
         drivetrain = new Drivetrain(hardwareMap, telemetry);
+        intake = new Intake(hardwareMap, telemetry);
+
+        intake.lockIntake();
+    }
+
+    @Override
+    public void start() {
+        intake.releaseIntake();
     }
 
     @Override
     public void loop() {
         drivetrain.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         drivetrain.addTelemetry(true);
+        intake.setIntake(gamepad1.a, gamepad1.b);
     }
 }
