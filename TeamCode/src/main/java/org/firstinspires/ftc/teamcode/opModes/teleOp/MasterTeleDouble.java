65beb611818cb72c22963848324f20df73d92c68
Tyler Smith <tjismith13@icloud.com>
2020-12-17 15:28:13 -0500

    Updated teleop for matches Friday Dec 18

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MasterTeleDouble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MasterTeleDouble.java
@@ -8,34 +8,52 @@ import org.firstinspires.ftc.teamcode.subsystems.robot.Robot;
 @TeleOp
 public class MasterTeleDouble extends OpMode {
 
+
     Robot robot;
-    double setpoint;
+    double shooterSetpoint;
+    boolean in;
+    boolean out;
 
     @Override
     public void init() {
         robot = new Robot(hardwareMap, telemetry);
-        setpoint = 0;
+        shooterSetpoint = 0;
     }
 
     @Override
     public void loop() {
+
+        //Control Drivetrain
         robot.drivetrain.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        if(gamepad2.right_trigger > 0.5) {
-            setpoint = 2500;
+
+        //Control Intake
+        if(gamepad2.a) {
+            in = true;
+            out = false;
         }
-        else if(gamepad2.left_trigger > 0.5) {
-            setpoint = -1000;
+        else if(gamepad2.b) {
+            out = true;
+            in = false;
         }
-        else if (gamepad2.dpad_up) setpoint = 1750;
-        else if (gamepad2.y) setpoint = 2000;
-        else if (gamepad2.x) setpoint = 2250;
         else {
-            robot.shooter.rawControl(0);
-            setpoint = 0;
+            in = false;
+            out = false;
         }
 
-        robot.shooter.update(setpoint);
-        robot.intake.update(gamepad2.a, gamepad2.b);
-        robot.clamp.update(gamepad2.left_bumper, gamepad2.right_bumper, gamepad2.dpad_up, gamepad2.dpad_down);
+        //Control shooter
+        if(gamepad2.y) shooterSetpoint = 2000;
+        else if(gamepad2.x) shooterSetpoint = 2500;
+        else if(gamepad2.left_bumper) {
+            shooterSetpoint = -500;
+            in = false;
+            out = true;
+        }
+        else {
+            shooterSetpoint = 0;
+            robot.shooter.rawControl(0);
+        }
+        robot.shooter.update(shooterSetpoint);
+        robot.intake.update(in, out);
+        //robot.clamp.update(gamepad1.left_bumper, gamepad1.right_bumper, gamepad1.dpad_up, gamepad1.dpad_down);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MasterTeleSingle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/MasterTeleSingle.java
@@ -9,17 +9,50 @@ import org.firstinspires.ftc.teamcode.subsystems.robot.Robot;
 public class MasterTeleSingle extends OpMode {
 
     Robot robot;
+    double shooterSetpoint;
+    boolean in;
+    boolean out;
 
     @Override
     public void init() {
         robot = new Robot(hardwareMap, telemetry);
+        shooterSetpoint = 0;
     }
 
     @Override
     public void loop() {
+
+        //Control Drivetrain
         robot.drivetrain.update(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        if(gamepad1.right_trigger > 0.5) robot.shooter.update(1);
-        robot.intake.update(gamepad1.a, gamepad1.b);
-        robot.clamp.update(gamepad1.left_bumper, gamepad1.right_bumper, gamepad1.dpad_up, gamepad1.dpad_down);
+
+        //Control Intake
+        if(gamepad1.a) {
+            in = true;
+            out = false;
+        }
+        else if(gamepad1.b) {
+            out = true;
+            in = false;
+        }
+        else {
+            in = false;
+            out = false;
+        }
+
+        //Control shooter
+        if(gamepad1.y) shooterSetpoint = 2000;
+        else if(gamepad1.x) shooterSetpoint = 2500;
+        else if(gamepad1.left_bumper) {
+            shooterSetpoint = -500;
+            in = false;
+            out = true;
+        }
+        else {
+            shooterSetpoint = 0;
+            robot.shooter.rawControl(0);
+        }
+        robot.shooter.update(shooterSetpoint);
+        robot.intake.update(in, out);
+        //robot.clamp.update(gamepad1.left_bumper, gamepad1.right_bumper, gamepad1.dpad_up, gamepad1.dpad_down);
     }
 }
