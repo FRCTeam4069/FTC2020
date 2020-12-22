bb0a15152b580ebbe4b123d53cbb7a565f7b2656
Tyler Smith <tjismith13@icloud.com>
2020-12-22 12:24:47 -0500

    Untested direct drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -63,6 +63,12 @@ public class Drivetrain extends RobotHardware {
     double brError;
 
     private double currentAvgPos;
+    double direction;
+
+    double totalTimeElapsed = 0;
+
+    //Save drivetrain state
+    Drivetrain.DriveState state = DriveState.NOT_DRIVING;
 
     //Setup such as initializing motors, setting directions and run modes
     public Drivetrain(HardwareMap hardwareMap, Telemetry telemetry) {
@@ -104,205 +110,250 @@ public class Drivetrain extends RobotHardware {
     //Update motors after calculating proper outputs
     public void update(double forward, double strafe, double turn) {
 
-        this.turn = turn;
+        if((state == DriveState.NOT_DRIVING || state == DriveState.HOLONOMIC) &&
+                (forward > 0.05 || strafe > 0.05 || turn > 0.05)) {
 
-        if (navx.isCalibrating()) {
-            telemetry.log().add("NavX Calibrating");
-            telemetry.update();
+            state = DriveState.HOLONOMIC;
+
+            this.turn = turn;
+
+            if (navx.isCalibrating()) {
+                telemetry.log().add("NavX Calibrating");
+                telemetry.update();
+            }
+
+            //Calculate direction and speed
+            double direction = Math.atan2(forward, strafe);
+            desiredSpeed = Math.hypot(forward, strafe);
+
+            //Calculate change in time
+            double currentTime = System.currentTimeMillis();
+            double elapsedTime = 0;
+            if (lastTime == 0) {
+                elapsedTime = 0;
+            } else {
+                elapsedTime = currentTime - lastTime;
+            }
+
+            totalTimeElapsed += elapsedTime;
+            lastTime = System.currentTimeMillis();
+
+
+            //Calculate change in position for each motor
+            double flCurrentPos = frontLeft.getCurrentPosition();
+            double blCurrentPos = backLeft.getCurrentPosition();
+            double frCurrentPos = frontRight.getCurrentPosition();
+            double brCurrentPos = backRight.getCurrentPosition();
+            double flPosChange;
+            double frPosChange;
+            double blPosChange;
+            double brPosChange;
+
+            if (flLastPos == 0) {
+                flPosChange = flCurrentPos;
+                blPosChange = blCurrentPos;
+                frPosChange = frCurrentPos;
+                brPosChange = brCurrentPos;
+            } else {
+                flPosChange = flCurrentPos - flLastPos;
+                blPosChange = blCurrentPos - blLastPos;
+                brPosChange = brCurrentPos - brLastPos;
+                frPosChange = frCurrentPos - frLastPos;
+            }
+
+            flLastPos = frontLeft.getCurrentPosition();
+            blLastPos = backLeft.getCurrentPosition();
+            frLastPos = frontRight.getCurrentPosition();
+            brLastPos = backRight.getCurrentPosition();
+
+            //calculate change in direction (turn)
+            currentTurn = navx.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+            if (currentTurn < 0) {
+                currentTurn = 180 + (180 - Math.abs(currentTurn));
+            }
+            if (lastTurn == 0) {
+                turnChange = 0;
+            } else {
+                turnChange = currentTurn - lastTurn;
+            }
+            lastTurn = navx.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+            if (lastTurn < 0) {
+                lastTurn = 180 + (180 - Math.abs(lastTurn));
+            }
+
+            if (!(turnChange > 180)) {
+                //PID turn
+                turnErrorSum = 0;
+                turnError = ((turnChange) - (turn));
+                turnErrorSum += turnError;
+
+                double turnP = 0.0015;
+                double turnI = 0.0;
+                double turnD = 0.00001;
+
+                turnOutput = turn + (turnError * turnP) + (turnErrorSum * turnI) + (turnChange * turnD);
+            } else turnOutput = turn;
+
+            //Calculate actual velocity of each motor
+            double flActualVelocity = flPosChange / elapsedTime;
+            double blActualVelocity = blPosChange / elapsedTime;
+            double frActualVelocity = frPosChange / elapsedTime;
+            double brActualVelocity = brPosChange / elapsedTime;
+
+            //Calculate desired motor outputs
+            desiredFrontLeftSpeed = (Math.sin(direction + Math.PI / 4) * desiredSpeed) + turnOutput;
+            desiredFrontRightSpeed = (Math.sin(direction - Math.PI / 4) * desiredSpeed) - turnOutput;
+            desiredBackRightSpeed = (Math.sin(direction + Math.PI / 4) * desiredSpeed) - turnOutput;
+            desiredBackLeftSpeed = (Math.sin(direction - Math.PI / 4) * desiredSpeed) + turnOutput;
+
+            //PID Outputs
+            flSum = 0;
+            frSum = 0;
+            blSum = 0;
+            brSum = 0;
+
+            flError = desiredFrontLeftSpeed - flActualVelocity;
+            frError = desiredFrontRightSpeed - frActualVelocity;
+            blError = desiredBackLeftSpeed - blActualVelocity;
+            brError = desiredBackRightSpeed - brActualVelocity;
+
+            flSum += flError;
+            frSum += frError;
+            blSum += blError;
+            brSum += brError;
+
+            double kP = 0.1;
+            double kI = 0.01;
+
+            frontLeftOutput = desiredFrontLeftSpeed + (flError * kP) + (kI * flSum);
+            frontRightOutput = desiredFrontRightSpeed + (frError * kP) + (kI * frSum);
+            backLeftOutput = desiredBackLeftSpeed + (blError * kP) + (kI * blSum);
+            backRightOutput = desiredBackRightSpeed + (brError * kP) + (kI * brSum);
+
+            //Check for speed scaling factor to counteract greater than 1 outputs
+            double max1 = Math.max((Math.abs(frontLeftOutput)), (Math.abs(frontRightOutput)));
+            double max2 = Math.max((Math.abs(backLeftOutput)), (Math.abs(backRightOutput)));
+            double scalar = Math.max(max1, max2);
+
+            if (scalar > 1) {
+                frontLeftOutput /= scalar;
+                frontRightOutput /= scalar;
+                backLeftOutput /= scalar;
+                backRightOutput /= scalar;
+            }
+
+            //Calculate average position for driveForwards and backwards commands
+            currentAvgPos = (((double) frontLeft.getCurrentPosition()) + ((double) frontRight.getCurrentPosition())
+                    + ((double) backLeft.getCurrentPosition()) + ((double) backRight.getCurrentPosition())) / 4;
+
+            //Set motor speeds
+            frontLeft.setPower(frontLeftOutput);
+            frontRight.setPower(frontRightOutput);
+            backLeft.setPower(backLeftOutput);
+            backRight.setPower(backRightOutput);
         }
-
-        //Calculate direction and speed
-        double direction = Math.atan2(forward, strafe);
-        desiredSpeed = Math.hypot(forward, strafe);
-
-        //Calculate change in time
-        double currentTime = System.currentTimeMillis();
-        double elapsedTime = 0;
-        if(lastTime == 0) {
-            elapsedTime = 0;
-        }
-        else {
-            elapsedTime = currentTime - lastTime;
-        }
-        double totalTimeElapsed = 0;
-        totalTimeElapsed += elapsedTime;
-        lastTime = System.currentTimeMillis();
-
-
-        //Calculate change in position for each motor
-        double flCurrentPos = frontLeft.getCurrentPosition();
-        double blCurrentPos = backLeft.getCurrentPosition();
-        double frCurrentPos = frontRight.getCurrentPosition();
-        double brCurrentPos = backRight.getCurrentPosition();
-        double flPosChange;
-        double frPosChange;
-        double blPosChange;
-        double brPosChange;
-
-        if(flLastPos == 0) {
-            flPosChange = flCurrentPos;
-            blPosChange = blCurrentPos;
-            frPosChange = frCurrentPos;
-            brPosChange = brCurrentPos;
-        }
-        else {
-            flPosChange = flCurrentPos - flLastPos;
-            blPosChange = blCurrentPos - blLastPos;
-            brPosChange = brCurrentPos - brLastPos;
-            frPosChange = frCurrentPos - frLastPos;
-        }
-
-        flLastPos = frontLeft.getCurrentPosition();
-        blLastPos = backLeft.getCurrentPosition();
-        frLastPos = frontRight.getCurrentPosition();
-        brLastPos = backRight.getCurrentPosition();
-
-        //calculate change in direction (turn)
-        currentTurn = navx.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
-        if(currentTurn < 0) {
-            currentTurn = 180 + (180 - Math.abs(currentTurn));
-        }
-        if(lastTurn == 0) {
-            turnChange = 0;
-        }
-        else {
-            turnChange = currentTurn - lastTurn;
-        }
-        lastTurn = navx.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
-        if(lastTurn < 0) {
-            lastTurn = 180 + (180 - Math.abs(lastTurn));
-        }
-
-        if(!(turnChange > 180)) {
-            //PID turn
-            turnErrorSum = 0;
-            turnError = ((turnChange) - (turn));
-            turnErrorSum += turnError;
-
-            double turnP = 0.0015;
-            double turnI = 0.0;
-            double turnD = 0.00001;
-
-            turnOutput = turn + (turnError * turnP) + (turnErrorSum * turnI) + (turnChange * turnD);
-        }
-        else turnOutput = turn;
-
-        //Calculate actual velocity of each motor
-        double flActualVelocity = flPosChange / elapsedTime;
-        double blActualVelocity = blPosChange / elapsedTime;
-        double frActualVelocity = frPosChange / elapsedTime;
-        double brActualVelocity = brPosChange / elapsedTime;
-
-        //Calculate desired motor outputs
-        desiredFrontLeftSpeed = (Math.sin(direction + Math.PI / 4) * desiredSpeed) + turnOutput;
-        desiredFrontRightSpeed = (Math.sin(direction - Math.PI / 4) * desiredSpeed) - turnOutput;
-        desiredBackRightSpeed = (Math.sin(direction + Math.PI / 4) * desiredSpeed) - turnOutput;
-        desiredBackLeftSpeed = (Math.sin(direction - Math.PI/4) * desiredSpeed) + turnOutput;
-
-        //PID Outputs
-        flSum = 0;
-        frSum = 0;
-        blSum = 0;
-        brSum = 0;
-
-        flError = desiredFrontLeftSpeed - flActualVelocity;
-        frError = desiredFrontRightSpeed - frActualVelocity;
-        blError = desiredBackLeftSpeed - blActualVelocity;
-        brError = desiredBackRightSpeed - brActualVelocity;
-
-        flSum += flError;
-        frSum += frError;
-        blSum += blError;
-        brSum += brError;
-
-        double kP = 0.1;
-        double kI = 0.01;
-
-        frontLeftOutput = desiredFrontLeftSpeed + (flError * kP) + (kI * flSum);
-        frontRightOutput = desiredFrontRightSpeed + (frError * kP) + (kI * frSum);
-        backLeftOutput = desiredBackLeftSpeed + (blError * kP) + (kI * blSum);
-        backRightOutput = desiredBackRightSpeed + (brError * kP) + (kI * brSum);
-
-        //Check for speed scaling factor to counteract greater than 1 outputs
-        double max1 = Math.max((Math.abs(frontLeftOutput)), (Math.abs(frontRightOutput)));
-        double max2 = Math.max((Math.abs(backLeftOutput)), (Math.abs(backRightOutput)));
-        double scalar = Math.max(max1, max2);
-
-        if(scalar > 1) {
-            frontLeftOutput /= scalar;
-            frontRightOutput /= scalar;
-            backLeftOutput /= scalar;
-            backRightOutput /= scalar;
-        }
-
-        //Calculate average position for driveForwards and backwards commands
-        currentAvgPos = (((double)frontLeft.getCurrentPosition()) + ((double)frontRight.getCurrentPosition())
-                + ((double)backLeft.getCurrentPosition()) + ((double)backRight.getCurrentPosition())) / 4;
-
-        //Set motor speeds
-        frontLeft.setPower(frontLeftOutput);
-        frontRight.setPower(frontRightOutput);
-        backLeft.setPower(backLeftOutput);
-        backRight.setPower(backRightOutput);
+        else state = DriveState.NOT_DRIVING;
     }
 
     public enum Direction {
         FORWARD,
         BACKWARD,
         LEFT,
-        RIGHT
+        RIGHT,
     }
 
     public void directDrive(Direction desiredDirection) {
 
-        double direction = 0;
-        if(desiredDirection == Direction.FORWARD) direction = Math.atan2(1, 0);
-        else if(desiredDirection == Direction.BACKWARD) direction = Math.atan2(-1, 0);
-        else if(desiredDirection == Direction.LEFT) direction = Math.atan2(0, -1);
-        else if(desiredDirection == Direction.RIGHT) direction = Math.atan2(0, 1);
+        if((state == DriveState.DIRECT_DRIVE || state == DriveState.NOT_DRIVING) && direction != 0) {
 
-        //Calculate change in time
-        double currentTime = System.currentTimeMillis();
-        double elapsedTime = 0;
-        if(lastTime == 0) {
-            elapsedTime = 0;
+            state = DriveState.DIRECT_DRIVE;
+
+            direction = 0;
+            if (desiredDirection == Direction.FORWARD) direction = Math.atan2(1, 0);
+            else if (desiredDirection == Direction.BACKWARD) direction = Math.atan2(-1, 0);
+            else if (desiredDirection == Direction.LEFT) direction = Math.atan2(0, -1);
+            else if (desiredDirection == Direction.RIGHT) direction = Math.atan2(0, 1);
+            else direction = 0;
+
+            //Calculate change in time
+            double currentTime = System.currentTimeMillis();
+            double elapsedTime = 0;
+            if (lastTime == 0) {
+                elapsedTime = 0;
+            } else {
+                elapsedTime = currentTime - lastTime;
+            }
+            double totalTimeElapsed = 0;
+            totalTimeElapsed += elapsedTime;
+            lastTime = System.currentTimeMillis();
+
+            //Calculate change in position for each motor
+            double flCurrentPos = frontLeft.getCurrentPosition();
+            double blCurrentPos = backLeft.getCurrentPosition();
+            double frCurrentPos = frontRight.getCurrentPosition();
+            double brCurrentPos = backRight.getCurrentPosition();
+            double flPosChange;
+            double frPosChange;
+            double blPosChange;
+            double brPosChange;
+
+            if (flLastPos == 0) {
+                flPosChange = flCurrentPos;
+                blPosChange = blCurrentPos;
+                frPosChange = frCurrentPos;
+                brPosChange = brCurrentPos;
+            } else {
+                flPosChange = flCurrentPos - flLastPos;
+                blPosChange = blCurrentPos - blLastPos;
+                brPosChange = brCurrentPos - brLastPos;
+                frPosChange = frCurrentPos - frLastPos;
+            }
+
+            flLastPos = frontLeft.getCurrentPosition();
+            blLastPos = backLeft.getCurrentPosition();
+            frLastPos = frontRight.getCurrentPosition();
+            brLastPos = backRight.getCurrentPosition();
+
+            double flVel = flPosChange / elapsedTime;
+            double frVel = frPosChange / elapsedTime;
+            double blVel = blPosChange / elapsedTime;
+            double brVel = brPosChange / elapsedTime;
+
+            desiredFrontLeftSpeed = Math.sin(direction + Math.PI / 4);
+            desiredFrontRightSpeed = Math.sin(direction - Math.PI / 4);
+            desiredBackRightSpeed = Math.sin(direction + Math.PI / 4);
+            desiredBackLeftSpeed = Math.sin(direction - Math.PI / 4);
+
+            flError = desiredFrontLeftSpeed - flVel;
+            frError = desiredFrontRightSpeed - frVel;
+            blError = desiredBackLeftSpeed - blVel;
+            brError = desiredBackRightSpeed - brVel;
+
+            flSum += flError;
+            frSum += frError;
+            blSum += blError;
+            brSum += brError;
+
+            double kP = 0.1;
+            double kI = 0.01;
+
+            frontLeftOutput = (flError * kP) + (flSum * kI);
+            frontRightOutput = (frError * kP) + (frSum * kI);
+            backLeftOutput = (blError * kP) + (blSum * kI);
+            backRightOutput = (brError * kP) + (brSum * kI);
+
+            frontLeft.setPower(frontLeftOutput);
+            frontRight.setPower(frontRightOutput);
+            backLeft.setPower(backLeftOutput);
+            backRight.setPower(backRightOutput);
         }
-        else {
-            elapsedTime = currentTime - lastTime;
-        }
-        double totalTimeElapsed = 0;
-        totalTimeElapsed += elapsedTime;
-        lastTime = System.currentTimeMillis();
+        else state = DriveState.NOT_DRIVING;
+    }
 
-        //Calculate change in position for each motor
-        double flCurrentPos = frontLeft.getCurrentPosition();
-        double blCurrentPos = backLeft.getCurrentPosition();
-        double frCurrentPos = frontRight.getCurrentPosition();
-        double brCurrentPos = backRight.getCurrentPosition();
-        double flPosChange;
-        double frPosChange;
-        double blPosChange;
-        double brPosChange;
-
-        if(flLastPos == 0) {
-            flPosChange = flCurrentPos;
-            blPosChange = blCurrentPos;
-            frPosChange = frCurrentPos;
-            brPosChange = brCurrentPos;
-        }
-        else {
-            flPosChange = flCurrentPos - flLastPos;
-            blPosChange = blCurrentPos - blLastPos;
-            brPosChange = brCurrentPos - brLastPos;
-            frPosChange = frCurrentPos - frLastPos;
-        }
-
-        flLastPos = frontLeft.getCurrentPosition();
-        blLastPos = backLeft.getCurrentPosition();
-        frLastPos = frontRight.getCurrentPosition();
-        brLastPos = backRight.getCurrentPosition();
-
-
+    public enum DriveState {
+        DIRECT_DRIVE,
+        HOLONOMIC,
+        NOT_DRIVING
     }
 
     @Override
