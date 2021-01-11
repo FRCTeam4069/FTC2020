abb970cb8332729d1e41c0000518bd9b4556985c
Tyler Smith <tjismith13@icloud.com>
2021-01-10 21:33:18 -0500

    Untested drop intake command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/commands/DropIntake.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.autonomous.commands;
+
+public class DropIntake extends Command {
+
+    double startingY;
+    double startingTime;
+    boolean isDone = false;
+    boolean firstForward = true;
+    double forwardTime;
+
+    @Override
+    public void start() {
+        startingY = robot.odometry.getYAvgPos();
+        startingTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void loop() {
+        if(System.currentTimeMillis() < startingTime + 1000)
+            robot.drivetrain.update(-1, 0, 0);
+        else {
+            if(firstForward) {
+                forwardTime = System.currentTimeMillis();
+                firstForward = false;
+            }
+            if(System.currentTimeMillis() < forwardTime + 750)
+                robot.drivetrain.update(1, 0, 0);
+            else isDone = true;
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return isDone;
+    }
+}
